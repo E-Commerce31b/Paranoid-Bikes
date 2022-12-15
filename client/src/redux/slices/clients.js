8945bb844e4d075c5fb840ebe34eb63e27cf33ee
German Horianski <germanezequielhorianski@gmail.com>
2022-12-14 23:31:46 -0300

    Creé las acciones de users

--- client/src/redux/slices/clients.js
+++ client/src/redux/slices/clients.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    clients: [],
-    client: {},
+    users: [],
+    user: {},
     logged: {},
     status: "",
     error: ""
@@ -19,38 +19,38 @@ export const clientsSlice = createSlice({
     extraReducers(builder) {
         builder 
             .addMatcher(
-            (action) => action.type.startsWith("clients/") && action.type.endsWith("/pending"),
+            (action) => action.type.startsWith("users/") && action.type.endsWith("/pending"),
             (state) => {state.status = 'loading'}
             )
             .addMatcher(
-            (action) => action.type.startsWith("clients/getClients") && action.type.endsWith("/fulfilled"),
+            (action) => action.type.startsWith("users/getUsers") && action.type.endsWith("/fulfilled"),
             (state, action) => {
                 state.status = 'succeeded'
                 state.clients = action.payload
             }
             )
             // .addMatcher(
-            //   (action) => action.type.startsWith("clients/getClient") && action.type.endsWith("/fulfilled"),
+            //   (action) => action.type.startsWith("users/getUser") && action.type.endsWith("/fulfilled"),
             //   (state, action) => {
             //     state.status = 'succeeded'
             //     state.client = action.payload
             //   }
             // )
             .addMatcher(
-            (action) => action.type.startsWith("clients/postClient"||"clients/putClient") && action.type.endsWith("/fulfilled"),
+            (action) => action.type.startsWith("users/postUser"||"clients/putUser") && action.type.endsWith("/fulfilled"),
             (state) => {
                 state.status = 'succeeded'
             }
             )
             .addMatcher(
-            (action) => action.type.startsWith("clients/deleteClient") && action.type.endsWith("/fulfilled"),
+            (action) => action.type.startsWith("users/deleteUser") && action.type.endsWith("/fulfilled"),
             (state, action) => {
                 state.status = 'succeeded'
                 state.clients = state.clients.filter(p => p.id !== action.payload.id)
             }
             )
             .addMatcher(
-            (action) => action.type.startsWith("clients/") && action.type.endsWith("/rejected"),
+            (action) => action.type.startsWith("users/") && action.type.endsWith("/rejected"),
             (state, action) => {
                 state.status = 'failed'
                 state.error = action.error.message
@@ -59,10 +59,10 @@ export const clientsSlice = createSlice({
         }, 
 });
 
-export const pacients = (state) => state.pacients
+export const pacients = (state) => state.users
 export const pacientsStatus = (state) => state.status
 export const pacientsError = (state) => state.error
-export const pacient = (state) => state.pacient
+export const pacient = (state) => state.user
 export const pacientStatus = (state) => state.status
 export const pacientError = (state) => state.error
 
