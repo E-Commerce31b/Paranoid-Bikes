340792928e6058436755b202a3944e6debbb17d0
German Horianski <germanezequielhorianski@gmail.com>
2022-12-15 17:55:41 -0300

    Terminé de conectar getProducts con el back

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import Stripe from "./components/Stripe/Stripe";
-
+import React from 'react'
+// import Stripe from "./components/Stripe/Stripe";
+// import { Route } from 'react-router'
+import ComponenteRedux from './components/componenteRedux.jsx'
 
 function App() {
   return (
-<Stripe/>  );
+        <ComponenteRedux/>
+);
 }
 
 export default App;
--- client/src/redux/slices/productsActions.js
+++ client/src/redux/slices/productsActions.js
@@ -1,27 +1,29 @@
 import axios from 'axios'
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const getProducts = createAsyncThunk('queries/getProducts', async () => {
+export const getProducts = createAsyncThunk('products/getProducts', async () => {
     try {
         // const response = await axios.get('https://api-paranoid-bikes-production.up.railway.app/api/bikes')
         const response = await axios.get('http://localhost:3001/api/bikes')
-        const data = response.data.data.sort(function(a, b) {
-            if(a.name < b.name) return -1;
-            if(a.name > b.name) return 1;
-            return 0
-        })
+        const data = response.data
+        console.log(response.data)
+        // .sort(function(a, b) {
+        //     if(a.name < b.name) return -1;
+        //     if(a.name > b.name) return 1;
+        //     return 0
+        // })
         return data.map(q => {
             return {
-                id:data._id,
-                name: data.name,
-                type: data.type,
-                created: data.createdDate,
-                genre: data.genre,
-                maker: data.maker, 
-                size: data.size,
-                price: data.price,
-                stock: data.stock,
-                image: data.image,
+                id:q._id,
+                name: q.name,
+                // type: q.type,
+                maker: q.maker, 
+                created: q.year,
+                gender: q.gender,
+                // size: q.size,
+                price: q.priceAmount,
+                // stock: q.stock,
+                image: q.image,
                 // isExpanded: false,
             }
         })
@@ -29,7 +31,7 @@ export const getProducts = createAsyncThunk('queries/getProducts', async () => {
         return error.message
     }        
 })
-export const getQueryById = createAsyncThunk('queries/getProductById', async (id) => {
+export const getQueryById = createAsyncThunk('products/getProductById', async (id) => {
     try {
         // const response = await axios.get(`https://api-paranoid-bikes-production.up.railway.app/api/bikes/${id}`)
         const response = await axios.get(`http://localhost:3001/api/bikes/${id}`)
@@ -52,9 +54,9 @@ export const getQueryById = createAsyncThunk('queries/getProductById', async (id
     }        
 })
 
-export const postQuery = createAsyncThunk('queries/postProduct', async (newBike) => {
+export const postQuery = createAsyncThunk('products/postProduct', async (newBike) => {
     try {
-        // const response = await axios.post('https://api-paranoid-bikes-production.up.railway.app/api/queries', newBike)
+        // const response = await axios.post('https://api-paranoid-bikes-production.up.railway.app/api/bikes', newBike)
         const response = await axios.post('http://localhost:3001/api/bikes', newBike)
         return response.data.data
     } catch (error) {
@@ -62,9 +64,9 @@ export const postQuery = createAsyncThunk('queries/postProduct', async (newBike)
     }        
 })
 
-export const putQuery = createAsyncThunk('queries/putProduct', async ({_id, ...query}) => {
+export const putQuery = createAsyncThunk('products/putProduct', async ({_id, ...query}) => {
     try {
-        // const response = await axios.put(`https://api-paranoid-bikes-production.up.railway.app/api/queries/${_id}`, query)
+        // const response = await axios.put(`https://api-paranoid-bikes-production.up.railway.app/api/bikes/${_id}`, query)
         const response = await axios.put(`http://192.168.0.215:3001/api/bikes/${_id}`, query)
         return response.data.data
     } catch (error) {
@@ -72,9 +74,9 @@ export const putQuery = createAsyncThunk('queries/putProduct', async ({_id, ...q
     }        
 })
 
-export const deleteQuery = createAsyncThunk('queries/deleteProduct', async (id) => {
+export const deleteQuery = createAsyncThunk('products/deleteProduct', async (id) => {
     try {
-        // const response = axios.delete(`https://api-paranoid-bikes-production.up.railway.app/api/queries/${id}`) 
+        // const response = axios.delete(`https://api-paranoid-bikes-production.up.railway.app/api/bikes/${id}`) 
         const response = axios.delete(`http://192.168.0.215:3001/api/bikes/${id}`) 
         return response.data.data
     } catch (error) {
