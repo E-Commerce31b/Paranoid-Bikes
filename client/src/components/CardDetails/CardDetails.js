e7eaffd128de2075b328a86f437b0c825c1acda7
Andres Garzon <agarzonsanchez90@gmail.com>
2022-12-16 07:17:31 -0500

    creacion carta bicicletas

--- client/src/components/CardDetails/CardDetails.js
+++ client/src/components/CardDetails/CardDetails.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CardDetails = () => {
-  return <h1>Hola mundo</h1>;
+  let bikes = useSelector((store) => store.products);
+  console.log(bikes);
+  return (
+    <div>
+      <p>Nombre de la bicicleta: </p>
+      <img src="" alt="Imagen bicicleta" />
+      <p>Modelo: </p>
+      <p>Año: </p>
+      <p>Nuevo/ Usado</p>
+      <p>Fabricante: </p>
+      <p>Genero: </p>
+      <p>Categoria: </p>
+      <p>eBike: </p>
+      <p>Precio: </p>
+    </div>
+  );
 };
 
 export default CardDetails;
