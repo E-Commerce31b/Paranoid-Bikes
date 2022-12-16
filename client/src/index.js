5969e8fb2f95ee006955df2e8a962b134d460e38
Andrea Barrera <andrea.barrera7@gmail.com>
2022-12-16 18:14:08 -0500

    Instalación de Bulma

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import "bulma/css/bulma.min.css";
+// import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
