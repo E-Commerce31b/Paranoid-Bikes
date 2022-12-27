81ae15b3c1dc7d44e4ad3bbbbd0d695cc39a6149
juanml08 <jmlopez-08@gotmail.com>
2022-12-27 13:57:14 -0500

    carrusel de publicidad y organizacion de algunas carpetas

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,18 @@ import Landing from "./components/Landing";
 import BestSellers from "./components/BestSellers";
 import Offers from "./components/Offers";
 import Record from "./components/Record";
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import UpdateProfile from "./components/UpdateProfile/UpdateProfile";
 import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/reusable/Header";
+import "./App.js";
 
 function App() {
   return (
     <AuthProvider>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/details/:id" element={<ProductDetail />} />
@@ -23,8 +26,8 @@ function App() {
         <Route path="/bestsellers" element={<BestSellers />} />
         <Route path="/offers" element={<Offers />} />
         <Route path="/record" element={<Record />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/update-profile" element={<UpdateProfile />} />
         <Route path="/stripe" element={<Stripe />} />
