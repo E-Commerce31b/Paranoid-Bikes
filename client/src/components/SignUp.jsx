f4052124b6633540d0103ef5d7c05ed480fcff95
Andres Garzon <agarzonsanchez90@gmail.com>
2022-12-28 09:51:15 -0500

    Merge branch 'develop' into detalle

---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,177 @@
+import React, { useRef, useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import logo from "../assets/Logo.png";
+import "../index.css";
+// import emailjs from "emailjs-com";
+
+export default function Signup() {
+  const form = useRef();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  const { signup, googleSignUp } = useAuth();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [boxState, setBoxState] = useState(false);
+  const navigate = useNavigate();
+
+  const changeState = () => {
+    setBoxState(true);
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordConfirmRef.current.value !== passwordRef.current.value)
+      return setError("Contraseña no coincide");
+
+    if (!boxState) return setError("Por favor aceptar terminos y condiciones");
+    try {
+      setError("");
+      setLoading(true);
+
+      await signup(
+        emailRef.current.value,
+        passwordRef.current.value,
+        passwordConfirmRef.current.value
+      );
+
+      // emailjs
+      //   .sendForm(
+      //     "service_ev9mv2j",
+      //     "template_hzyfavr",
+      //     form.current,
+      //     "gYTIZ320UzKrK9phD"
+      //   )
+      // .then(
+      //   (result) => {
+      //     console.log(result.text);
+      //   },
+      //   (error) => {
+      //     console.log(error.text);
+      //   }
+      // );
+
+      e.target.reset();
+      setBoxState(false);
+      navigate("/user"); /// cambiar a ruta user
+    } catch {
+      setError("Error al crear la cuenta");
+    }
+  }
+
+  async function googleSubmit(e) {
+    e.preventDefault();
+    if (!boxState) return setError("Por favor aceptar terminos y condiciones");
+    try {
+      setError("");
+      setLoading(true);
+      await googleSignUp();
+      navigate("/user"); /// cambiar a ruta user
+    } catch {
+      setError("Error al crear la cuenta");
+    }
+  }
+
+  return (
+    <div className="backgroundColor">
+      <div style={{ backgroundColor: "white" }}>
+        <div className="navbar-brand ">
+          <div className="navbar-item ">
+            <NavLink to="/home " className="textDecoration">
+              <img src={logo} alt="logo" width="112" height="80" />
+              <div className=" is-size-4 has-text-weight-bold font_family is-pulled-right">
+                <h1 className="flex has-text-right ">
+                  <span>Paranoid </span>
+                  <span>Bikes</span>
+                </h1>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ margin: "80px" }}>
+        <h1 className="column has-text-centered" style={{ paddingTop: "10px" }}>
+          <strong className="font_family fontColor">CREAR USUARIO</strong>
+        </h1>
+      </div>
+
+      <div
+        className="container box"
+        style={{ width: "50%", marginTop: "50px" }}
+      >
+        {error && <p className="notification is-danger is-light">{error}</p>}
+
+        <form ref={form} onSubmit={handleSubmit}>
+          <div className="field">
+            <label className="label font_family">Nombre</label>
+            <input className="input" type="text" name="name"></input>
+          </div>
+          <div className="field">
+            <label className="label font_family">Correo electronico</label>
+            <input
+              className="input"
+              type="email"
+              name="email"
+              ref={emailRef}
+            ></input>
+          </div>
+
+          <div className="field">
+            <label className="label font_family">Contraseña</label>
+            <input className="input" type="password" ref={passwordRef}></input>
+          </div>
+
+          <div className="field">
+            <label className="label font_family">Confirmar contraseña</label>
+            <input
+              className="input"
+              type="password"
+              ref={passwordConfirmRef}
+            ></input>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <label className="checkbox">
+                <input type="checkbox" onChange={changeState} />
+                <a href="#" className="font_family">
+                  Acepto los terminos y condiciones
+                </a>
+              </label>
+            </div>
+          </div>
+
+          <div className="buttons has-text-centered">
+            <button className="button is-primary font_family" type="submit">
+              Registrarse
+            </button>
+
+            <Link to="/">
+              <button className="button is-light font_family" type="submit">
+                Cancelar
+              </button>
+            </Link>
+          </div>
+        </form>
+        <br />
+
+        <button
+          className="button is-warning font_family"
+          type="submit"
+          onClick={googleSubmit}
+        >
+          Registrarse con Google
+        </button>
+        <div className="font_family">
+          Ya tiene una cuenta con nosotros?{" "}
+          <Link to="/login">Iniciar Sesion</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
