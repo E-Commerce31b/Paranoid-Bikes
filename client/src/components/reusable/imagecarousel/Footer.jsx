12ea666f51ce3752ef4eb08bae298e03c621c1c6
juanml08 <jmlopez-08@gotmail.com>
2023-01-02 16:35:30 -0500

    navegacion componentes de metodo de pago, pedidos y perfil, implementacion de footer

---
+++ client/src/components/reusable/imagecarousel/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="position_footer">
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>PARANOID BIKE'S </strong>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
