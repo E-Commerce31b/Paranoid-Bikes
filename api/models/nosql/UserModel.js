87ba034ff84321ccc80f5cffce428199c485c3d6
rodrisantes <104109457+rodrisantes@users.noreply.github.com>
2023-01-04 21:41:12 -0300

    arregloModeluser

--- api/models/nosql/UserModel.js
+++ api/models/nosql/UserModel.js
@@ -68,61 +68,7 @@ UserScheme.pre("save", function (next) {
     });
   } else {
     next();
-    const UserScheme = new mongoose.Schema({
-      id: {
-        type: mongoose.Types.ObjectId,
-      },
-      first_name: {
-        type: String,
-      },
-      last_name: {
-        type: String,
-      },
-      history: {
-        type: Array,
-      },
-      type: {
-        type: String,
-      },
-      purchased: {
-        type: Array,
-      },
-      email: {
-        type: String,
-      },
-      country: {
-        type: String,
-      },
-      city: {
-        type: String,
-      },
-      state: {
-        type: String,
-      },
-      address: {
-        type: String,
-      },
-      birthday: {
-        type: String,
-      },
-      DNI: {
-        type: Number,
-      },
-      password: {
-        type: String,
-      },
-      cart: {
-        type: Array,
-      },
-      favorites: {
-        type: Array,
-      },
-      softDelete: {
-        type: Boolean,
-        default: false,
-      },
-    });
-  }
+      }
 });
 
 UserScheme.methods.isCorrectPassword = function (password, callback) {
