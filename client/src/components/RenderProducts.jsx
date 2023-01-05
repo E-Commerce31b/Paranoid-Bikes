3a9b7293b6c0efd8f23ec778627a0fb5c5750adc
Andres Garzon <agarzonsanchez90@gmail.com>
2023-01-05 14:59:03 -0500

    detalles bicicletas funcionando

--- client/src/components/RenderProducts.jsx
+++ client/src/components/RenderProducts.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+import React from "react";
+import ProductCard from "./ProductCard";
 
-const RenderProducts = ({slicedProducts}) => {
-    
-    return (
-        <div style={{display:"flex", flexWrap:"wrap", alignItems:"center", gap:"10px"}}>{
-            slicedProducts().map((p,i) => {
-                return (<div key={i}>
-                    <ProductCard product={p}/></div>)})
-            }</div>
-    )
-}
+const RenderProducts = ({ slicedProducts }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        gap: "10px",
+      }}
+    >
+      {slicedProducts().map((p, i) => {
+        return (
+          <div key={i}>
+            <ProductCard product={p} />
+            {console.log(p)}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default RenderProducts
+export default RenderProducts;
