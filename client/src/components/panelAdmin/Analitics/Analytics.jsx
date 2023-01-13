563042c02fa3bfcaa968ef78b81f8f246ad1c4a6
faculpez <faculpez1998@gmail.com>
2023-01-13 16:17:21 -0300

    Cards analitica Sales de panel de Admin

--- client/src/components/panelAdmin/Analitics/Analytics.jsx
+++ client/src/components/panelAdmin/Analitics/Analytics.jsx
@@ -1,5 +1,25 @@
-import React from "react";
 
-export default function Analytics() {
-  return <div>Analytics</div>;
-}
+
+export function salesValue (props) {
+      if(props) {
+        let total = props.reduce(function(accumulator, currentValue) {
+          let pre = parseInt(currentValue.price)
+          return accumulator + (pre * currentValue.count);
+        }, 0);
+        return total
+      }}
+
+export function salesBarValue(props) {
+  if(props) {
+    let count = props.reduce(function(accumulator, currentValue) {
+      return accumulator + currentValue.count;
+    }, 0);
+    // console.log(count);
+    let stock = props.reduce(function(accumulator, currentValue) {
+      return accumulator + currentValue.stock;
+    }, 0);
+    // console.log(stock);
+    let percentage = Math.ceil((count / stock) * 100);
+    // console.log(percentage);
+    return percentage
+}}
