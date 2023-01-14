151d72f5b7c9a8154e7cd5e2d780ed59a3b045bc
SebaAguiar <sebaaguiar77@gmail.com>
2023-01-13 21:22:50 -0300

    salvame por favor superman

--- client/src/index.js
+++ client/src/index.js
@@ -11,11 +11,11 @@ import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
