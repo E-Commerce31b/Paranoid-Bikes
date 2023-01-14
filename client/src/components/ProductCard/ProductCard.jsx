ebb921baf6ed8788f3bf0c55324c0e926717a451
faculpez <faculpez1998@gmail.com>
2023-01-14 18:23:12 -0300

    Buttons VER MAS y carrito en las cards

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './productCard.css';
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Button, ButtonGroup } from "@mui/material";
+import { useDispatch, useSelector } from 'react-redux';
+import { getProduct } from '../../redux/slices/productsActions';
+import { managePurchased } from '../../redux/slices/users';
+
 
 const ProductCard = ({product}) => {
+  let { id } = useParams();
+  const dispatch = useDispatch();
+
+  const [bike, setBike] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const user = useSelector(state => state.users.user)
+  const products = useSelector (state => state.products.products)
+
+  useEffect(() => {
+    const data = async () => {
+      setLoading(true);
+      const data = await dispatch(getProduct(id));
+      setBike(data.payload);
+      setLoading(false);
+    };
+    data();
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+    const sendToCart = () => {
+    if(user) {
+      dispatch(managePurchased({id, products}))
+      console.log(user)
+      // dispatch(putUser(user._id, user))}
+      }
+    }
 
     return (
-        <Link to={`/details/${product.id}`}>
+        <>
         <div className='box'>
             <div className='box-image'>
             <img src={product.image} alt="Not found" />
@@ -14,8 +49,18 @@ const ProductCard = ({product}) => {
             <div className='name-price'>
                 <p><strong>Nombre:</strong> {product.name}</p>
                 <p><strong>Precio:</strong> $ {product.price}</p></div>
+            <ButtonGroup className="botones">
+                <Link to={`/details/${product.id}`}>
+                    <Button>
+                        <h1 fontSize='30'>Ver más</h1>
+                    </Button>
+                </Link>
+                <Button onClick={() => sendToCart()}>
+                    <h1 fontSize='30'>🛒</h1>
+                </Button>
+                </ButtonGroup>
         </div>
-        </Link>
+        </>
     )
 }
 
