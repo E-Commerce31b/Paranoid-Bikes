6775cab58adb404b4ceb5acb2bb0aed30ce34675
Andrea Barrera <andrea.barrera7@gmail.com>
2023-01-16 11:41:06 -0500

    Estilos boton de proceder a compra

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,48 +1,54 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import RenderProducts from './RenderProducts'
-import { useNavigate } from 'react-router'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { useSelector } from "react-redux";
+import RenderProducts from "./RenderProducts";
+import { useNavigate } from "react-router";
+import { NavLink } from "react-router-dom";
 
 const Cart = () => {
+  const selected = useSelector((state) => state.users.user.purchased);
+  const currentPage = useSelector((state) => state.products.currentPage);
+  const user = useSelector((state) => state.users.user);
 
-    const selected = useSelector(state => state.users.user.purchased)
-    const currentPage = useSelector((state) => state.products.currentPage);
-    const user = useSelector((state) => state.users.user)
+  // const currentPage = useSelector((state) => state.products.currentPage);
 
-    // const currentPage = useSelector((state) => state.products.currentPage);
-  
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handlePayment = () => {
-      navigate('/stripe', {state: {selected: selected}})
+  const handlePayment = () => {
+    navigate("/stripe", { state: { selected: selected } });
+  };
+
+  const slicedProducts = () => {
+    // if(product) return product;
+    if (selected) {
+      // return filtered.slice(currentPage, currentPage ? currentPage + 16 : 0);
+      return selected;
+      // .slice(currentPage, currentPage + 16);
     }
-
-    const slicedProducts = () => {
-      // if(product) return product;
-      if (selected) {
-        // return filtered.slice(currentPage, currentPage ? currentPage + 16 : 0);
-        return selected
-        // .slice(currentPage, currentPage + 16);
-      }
-    };
-    return (
+  };
+  return (
+    <div>
+      {selected.length > 0 ? (
         <div>
-          {
-            (selected.length > 0) 
-            ? 
-            <div>
-              <RenderProducts slicedProducts={slicedProducts} />
-              <button onClick={() => handlePayment()}>Proceder a la compra</button>
-            </div>
-            : 
-            <div>
-              <p>No hay productos seleccionados.</p>
-              <NavLink to='/'><button>Agregar productos</button></NavLink>
-            </div>
-          }
+          <RenderProducts slicedProducts={slicedProducts} />
+          <div className="columns is-centered mb-3 mt-2">
+            <p
+              className="button is-primary font_family"
+              onClick={() => handlePayment()}
+            >
+              Proceder a la compra
+            </p>
+          </div>
         </div>
-    )
-}
+      ) : (
+        <div>
+          <p>No hay productos seleccionados.</p>
+          <NavLink to="/">
+            <button>Agregar productos</button>
+          </NavLink>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
