3eb73e5971e30951fe037631beb0a7925842b624
amma2win <102752491+amma2win@users.noreply.github.com>
2023-01-18 21:20:45 -0300

    falta el post de bicis

--- client/src/components/panelAdmin/BikeCreate/BikeCreate.jsx
+++ client/src/components/panelAdmin/BikeCreate/BikeCreate.jsx
@@ -1,12 +1,15 @@
-
+import React, { useRef, useState } from "react";
 import { useForm, Controller, set } from "react-hook-form";
 import { postProduct } from "../../../../src/redux/slices/productsActions";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 export const validate = (input) => {
   let errors = {};
   if (!input.name) {
     errors.name = "ingrese nombre de bici";
   }
+  return errors;
 };
 export default function BikeCreate() {
   const form = useRef();
@@ -19,12 +22,16 @@ export default function BikeCreate() {
     maker: "",
     img: "",
     priceAmount: "",
+    year:"",
   });
   const [formErrors, setFormErrors] = useState({
     name: "",
-    genre: "",
+    gender: "",
     category: "",
     maker: "",
+    img: "",
+    priceAmount: "",
+    year:"",
   });
   const navigate = useNavigate();
   const handleInputChange = (e) => {
@@ -71,42 +78,98 @@ export default function BikeCreate() {
   };
   console.log("input", input);
 
-  return (
-    <div>
-      <form ref={form} onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label font_family">Nombre</label>
-          <input
-            className="input"
-            type="name"
-            name="name"
-            /*  ref={emailRef} */
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div className="field">
-          <label className="label font_family">Genero</label>
-          <input
-            className="input"
-            type="genre"
-            name="genre"
-            /*  ref={emailRef} */
-            onChange={handleInputChange}
-          ></input>
-        </div>
-        <div className="buttons has-text-centered">
-          <button className="button is-primary font_family" type="submit">
-            Registrarse
-          </button>
+  return ( 
+     <div className=" mb-6 ">
+  <div style={{ backgroundColor: "white" }}>
+    <div className="navbar-brand "></div>
+  </div>
 
-          <Link to="/">
-            <p className="button is-light font_family" type="submit">
-              Cancelar
-            </p>
-          </Link>
-        </div>
-      </form>
+  <div className="column has-text-centered">
+    <h1 className="title is-3 font_family mt-5">Agregar bicicletas</h1>
+  </div>
+  <div className="columns">
+    <div className="column is-3"></div>
+    <div
+      className="column is-6 mt-5 has-background-white"
+      style={{ borderRadius: "10px" }}
+    >
       <div>
+        {error && (
+          <p className="notification is-danger is-light ">{error}</p>
+        )}
+        {formErrors.name && (
+          <p className="is-size-7-desktop notification is-danger is-light ">
+            {formErrors.name}
+          </p>
+        )}
+        {formErrors.gender && (
+          <p className="is-size-7-desktop notification is-danger is-light">
+            {formErrors.gender}
+          </p>
+        )}
+        <form ref={form} onSubmit={handleSubmit}>
+          <div className="field">
+            <label className="label font_family">Nombre</label>
+            <input
+              className="input"
+              type="name"
+              name="name"
+             
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="field">
+            <label className="label font_family">Genero</label>
+            <input
+              className="input"
+              type="gender"
+              name="gender"
+            
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="field">
+            <label className="label font_family">Categoria</label>
+            <input
+              className="input"
+              type="category"
+              name="category"
+              
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="field">
+            <label className="label font_family">Marca</label>
+            <input
+              className="input"
+              type="maker"
+              name="maker"
+              /*  ref={emailRef} */
+              onChange={handleInputChange}
+            ></input>
+          </div>
+          <div className="field">
+            <label className="label font_family">Precio</label>
+            <input
+              className="input"
+              type="priceAmount"
+              name="priceAmount"
+             
+              onChange={handleInputChange}
+            ></input>
+          </div>
+
+          <div className="field">
+            <label className="label font_family">Fecha de lanzamiento</label>
+            <input
+              className="input"
+              type="year"
+            
+              onChange={handleInputChange}
+              name="year"
+            ></input>
+          </div>
+          <div>
         <input
           type="file"
           className="input"
@@ -114,8 +177,32 @@ export default function BikeCreate() {
           onChange={(e) => setImage(e.target.files[0])}
         />
         <button onClick={submitImage}>Subir Imagen</button>
+        </div>
+          
+
+         
+
+          <div className="buttons has-text-centered">
+            <button className="button is-primary font_family" type="submit">
+              Agregar Bici
+            </button>
+
+            <Link to="/">
+              <p className="button is-light font_family" type="submit">
+                Cancelar
+              </p>
+            </Link>
+          </div>
+         
+        </form>
+        <br />
+      
       </div>
     </div>
+    </div>
+        </div>
+     
+    
   );
 }
 
