d7604e3ea66de201aefd731f5ebf1f2a9b0a32a1
German Horianski <germanezequielhorianski@gmail.com>
2023-01-23 20:41:14 -0300

    Agregué botón para seguir agregando productos al carrito

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -53,6 +53,9 @@ const Cart = () => {
           <RenderProducts slicedProducts={slicedProducts} />
           <div className="cart_container">
             <div className="flex is-flex-direction-row ">
+            <NavLink to='/'>
+                <p className="font-family">Agregar más productos al carrito</p>
+            </NavLink>
               <p className="is-size-3 px-6 has-text-primary">TOTAL A PAGAR =</p>
               <p className="is-size-3 has-text-primary"> {total}</p>
             </div>
