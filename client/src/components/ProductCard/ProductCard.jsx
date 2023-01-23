224cc79dd9a3679897da7345d016b219d04371f1
Andrea Barrera <andrea.barrera7@gmail.com>
2023-01-23 15:02:55 -0500

    Animaciones completas de cards y productDetail

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -3,26 +3,27 @@ import "./productCard.css";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { putUserCart, removeFromCart } from "../../redux/slices/usersActions.js";
+import {
+  putUserCart,
+  removeFromCart,
+} from "../../redux/slices/usersActions.js";
 import Counter from "../Counter";
 
 import IconButton from "@mui/material/IconButton";
 import iconDelete from "../../assets/iconDelete.png";
 
 const ProductCard = ({ product }) => {
-
   const [counter, setCounter] = useState(1);
 
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.products.products)
+  const products = useSelector((state) => state.products.products);
   const navigate = useNavigate();
   const params = useLocation();
   const user = useSelector((state) => state.users.user);
 
   const sendToCart = (action) => {
     if (Object.keys(user).length) {
-
-      dispatch(putUserCart({ product, user, action: 'increment'}));
+      dispatch(putUserCart({ product, user, action: "increment" }));
       return navigate("/cart");
     } else {
       return navigate("/login");
@@ -32,58 +33,59 @@ const ProductCard = ({ product }) => {
   return (
     <>
       <div className="box flex is-justify-content-center px-6 boxCard">
-      {params?.pathname === "/cart" ? 
+        {params?.pathname === "/cart" ? (
           <div className="icon_container">
             <div onClick={() => dispatch(removeFromCart({ product, user }))}>
               <IconButton aria-label="delete" size="small">
-                  <img src={iconDelete} alt="iconDelete" width={21} />
+                <img src={iconDelete} alt="iconDelete" width={21} />
               </IconButton>
             </div>
-        </div>
-            : 
-            <></>
-          }
+          </div>
+        ) : (
+          <></>
+        )}
         <div className="box-image py-5">
           <img className="imagen" src={product.image} alt="Not found" />
         </div>
         <div className="maker">
-          <strong>Marca:</strong> {product.maker}
+          <strong className="p_detail">Marca:</strong> {product.maker}
         </div>
         <div title={product.name}>
           <p className="name-price">
-            <strong>Nombre:</strong> {product.name}
+            <strong className="p_detail">Nombre:</strong> {product.name}
           </p>
           <p>
-            <strong>Precio:</strong> $ {product.priceAmount}
+            <strong className="p_detail">Precio:</strong> ${" "}
+            {product.priceAmount}
           </p>
-          {params?.pathname === "/cart" ? 
+          {params?.pathname === "/cart" ? (
             <p>
               <strong>Subtotal:</strong> $ {product.priceAmount * product.count}
             </p>
-            : 
+          ) : (
             <></>
-          }
+          )}
         </div>
         {params?.pathname === "/cart" ? (
-            <div className="pt-5 flex   ">
+          <div className="pt-5 flex   ">
             <div className="flex is-flex-direction-row is-justify-content-space-between p-3">
               <Link to={`/details/${product.id}`}>
                 <Button variant="outlined">Ver más</Button>
               </Link>
             </div>
           </div>
-          ) : (
-            <div className="pt-5 flex   ">
-              <div className="flex is-flex-direction-row is-justify-content-space-between p-3">
-                <Link to={`/details/${product.id}`}>
-                  <Button variant="outlined">Ver más</Button>
-                </Link>
-                <Button variant="outlined" onClick={() => sendToCart()}>
-                  🛒
-                </Button>
-              </div>
+        ) : (
+          <div className="pt-5 flex   ">
+            <div className="flex is-flex-direction-row is-justify-content-space-between p-3">
+              <Link to={`/details/${product.id}`}>
+                <Button variant="outlined">Ver más</Button>
+              </Link>
+              <Button variant="outlined" onClick={() => sendToCart()}>
+                🛒
+              </Button>
             </div>
-          )}
+          </div>
+        )}
         <div className="flex is-justify-content-space-around is-flex-direction-row py-5">
           {params?.pathname === "/cart" ? (
             <Counter
