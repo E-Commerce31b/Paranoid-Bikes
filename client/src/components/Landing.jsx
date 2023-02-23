e1b2a0a1d6b2e36300a7c943c5a4dd0fae071b06
German Horianski <germanezequielhorianski@gmail.com>
2023-02-23 17:54:26 -0300

    Redesign of landing page, with new commited components for future usage

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,31 +1,50 @@
 import React from "react";
 import ImageDiscount from "../assets/Imagedescuentos.png";
-import CategoryCard from "./CategoryCard.jsx";
 import Loader from "./Loader";
 import { useSelector } from "react-redux";
+import BestSellers from "./BestSellers";
+import './Landing.css';
+import Categories from './Categories'
+import BikeAdvisorIntro from './BikeAdvisorIntro'
+import NewsLetter from './NewsLetter'
+import Discord from './Discord'
+import LandingHeader from './LandingHeader'
 
 export default function Landing() {
-  const categories = useSelector((state) => state.products.categories);
-  const status = useSelector((state) => state.products.status);
+
+  // const status = useSelector((state) => state.products.status);
+
+  // <Loader loading={status}></Loader>
 
   return (
     <div>
-      <Loader loading={status}></Loader>
-      <div className="overlay">
-        <img src={ImageDiscount} alt="not found" />
+      <LandingHeader />
+      <div className="landing_subsection_preferences">
+        <h2 className="py-6 has-text-centered has-text-white">
+            At Paranoid, we understand that everyone has different cycling goals and preferences. 
+        </h2>
       </div>
-      <h2 className="is-size-3 py-6 has-text-centered has-text-white">
-        Categorías
-      </h2>
-      <div className="flex is-flex-direction-row is-justify-content-space-evenly is-flex-wrap-wrap pb-6">
-        {categories?.map((c, i) => {
-          return (
-            <div key={i}>
-              <CategoryCard category={c} />
-            </div>
-          );
-        })}
+        <Categories/>
+      {/* <div className="landing_subsection_bike_advisor">
+        <h2> 
+            No goals nor preferences, but need some guidance? 
+        </h2>
+        <h2 className="bigger"> 
+            We got you 
+        </h2>
+        <BikeAdvisorIntro/>
       </div>
+      <div className="bestSellers_container">
+        <BestSellers />
+      </div>
+      <div>
+        <p>Stay ahead of Paranoid offers and news regarding the world of bikes:</p>
+        <NewsLetter />
+      </div>
+      <div>
+        <p>Share your passion by joining our Discord server:</p>
+        <Discord />
+      </div> */}
     </div>
   );
 }
