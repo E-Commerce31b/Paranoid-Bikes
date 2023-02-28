deb797eb1068fcd407c485fcdfc51bd7fd350473
German Horianski <germanezequielhorianski@gmail.com>
2023-02-27 21:19:40 -0300

    Lazy Loading for Categories

---
+++ client/src/components/Categories/index.jsx
@@ -0,0 +1,19 @@
+import React, {Suspense} from 'react'
+import useNearScreen from '../CustomHooks/useNearScreen.jsx';
+import Loader from '../Loader'
+
+const Categories = React.lazy(
+    () => import('./Categories.jsx')
+)
+
+export default function LazyCategories () {
+    const {isNearScreen, fromRef} = useNearScreen();
+
+    {/* <Suspense fallback={<Loader />}> */}        //conviene no envolver la app en Suspense, tener ctrol más granular, y fallbacks personalizados (incluso React Placeholders SVG)
+    return (
+        <div ref={fromRef}>
+                {isNearScreen ? <Categories /> : <Loader />}
+            {/* </Suspense> */}
+        </div>
+    )
+}
